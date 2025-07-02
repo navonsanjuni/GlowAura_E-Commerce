a1fa0323ca70f19a166de028675b53f8f28ebd01
navonsanjuni <navonsanjuni178@gmail.com>
2025-07-02 14:46:40 +0530

    Add JWT Authentication

--- cbc-backend/index.js
+++ cbc-backend/index.js
@@ -1,12 +1,11 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose from 'mongoose';
-
-import Student from './models/student.js'; 
 import studentRouter from './routes/studentRouter.js'; 
 import productRouter from './routes/productRouter.js';
 import userRouter from './routes/userRouter.js'; 
-
+import jwt from 'jsonwebtoken';
+import e from 'express';
 
 
  const app = express();
@@ -22,13 +21,29 @@ const connection = mongoose.connection;
 
  app.use(bodyParser.json());
 
+ app.use(
+    (req, res, next) => {
+        const token = req.header('Authorization')?.replace('Bearer ', '')
+        console.log(token);
+
+        if(token !=null){
+            jwt.verify(token, 'glowra-secret-key-7973', (error, decoded) => {
+                if(!error){
+                    console.log(decoded);
+                    req.user = decoded;
+                }
+        })
+    }
+    next();
+})
+
  app.use('/api/students', studentRouter); 
 
  app.use('/api/products', productRouter);
 
  app.use('/api/users', userRouter);
 
- app.use('/users', userRouter); 
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
